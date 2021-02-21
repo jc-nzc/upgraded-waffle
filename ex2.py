0a3ba9733ac82870ec630d4e3e0e0a52e3efb751
Zero <node.zerocool@gmail.com>
2021-02-21 13:44:42 -0600

    comment practice

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# Comments, so we can read the program later
+# Anything after the comment is ignored
+
+print("I could have code like this") # and comments after the statement
+
+# You can also use a comment to "disable" code or comment out code:
+# print("this won't run")
+
+print("this will run")
