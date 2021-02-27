0f7261f04bf8de2c27786a20c3cd069978422c4a
Zero <node.zerocool@gmail.com>
2021-02-27 10:07:57 -0600

    string concatentation

---
+++ ex7.py
@@ -0,0 +1,20 @@
+print("Donut had 15 toys.")
+print("His fur was black and white.")
+print("And all day he played with his toys.")
+print("Until he falls to sleep.")
+print("." * 7) #guess what this does
+
+end1 = "E"
+end2 = "n"
+end3 = "g"
+end4 = "i"
+end5 = "n"
+end6 = "e"
+end7 = "e"
+end8 = "r"
+end9 = "i"
+end10 = "n"
+end11 = "g"
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end="")
+print(end7 + end8 + end9 + end10 + end11)
