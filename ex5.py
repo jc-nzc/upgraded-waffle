340d062ca19d08cdb88948fecec8d013d362a8e0
Zero <node.zerocool@gmail.com>
2021-02-26 22:42:37 -0600

    round up/down test

--- ex5.py
+++ ex5.py
@@ -16,3 +16,6 @@ print(f"His teeth are usually {my_teeth} depending on the coffee.")
 # this line is tricky, try to get it exactly right
 total = my_age + my_height + my_weight
 print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+age = 420.6
+print(round(age))
