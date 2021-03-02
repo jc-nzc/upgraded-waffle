bae798a54a107b4da0d1d80b269e19329306dc8d
Zero <node.zerocool@gmail.com>
2021-03-01 23:24:55 -0600

    list exer

---
+++ list.py
@@ -0,0 +1,12 @@
+thislist = ["jac", "df", "agv"]
+
+print(thislist[2])
+
+print(len(thislist))
+
+print(type(thislist))
+
+
+thatlist = list(("peanuts", "tofu", "fruit rollup")) # note the double round-brackets
+
+print(thatlist)
