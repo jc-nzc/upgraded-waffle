a63a8c5662a1c47530f6672be373a60cb48c5599
Zero <node.zerocool@gmail.com>
2021-03-01 23:26:30 -0600

    lists cont

--- list.py
+++ list.py
@@ -1,6 +1,11 @@
 thislist = ["jac", "df", "agv"]
 
 print(thislist[2])
+print(thislist[-1])
+print(thislist[1:4])
+print(thislist[:2])
+if "agv" in thislist:
+  print("Yes, 'Ale is present' is in the fruits list")
 
 print(len(thislist))
 
