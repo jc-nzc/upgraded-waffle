1ade9591abc705123bb95ce669df5343c85368f9
Zero <node.zerocool@gmail.com>
2021-03-07 07:43:17 -0600

    zip list

--- list.py
+++ list.py
@@ -68,3 +68,12 @@ for x in rhymes:
     newlist.append(x)
 
 print(newlist)
+
+
+print("____ zip lists exercises")
+
+letters = ['a', 'b', 'c']
+numbers = [0, 1, 2]
+for l, n in zip(letters, numbers):
+    print(f'Letter: {l}')
+    print(f'Number: {n}')
