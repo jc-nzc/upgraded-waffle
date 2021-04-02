61c527dcb75f97412b3fe0640c1a0522f65e750e
Zero <node.zerocool@gmail.com>
2021-04-01 22:56:48 -0500

    str formatting

--- class.py
+++ class.py
@@ -9,3 +9,18 @@ class SecondaryClass:
 
 p2 = SecondaryClass()
 print(p2.m, p1.x)
+
+types_of_people = 10
+df = f"There are {types_of_people} types of people"
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+
+print(df)
+print(y)
+
+
+price = 49
+txt = "The price is {} dollars"
+print(txt.format(price))
